d26806de255ee4f39ae7007861efbe5ce771680d
Romteix <git.unsocial052@passinbox.com>
2024-07-25 08:48:13 +0200

    correction du formulaire de login

--- app/view/view_login.php
+++ app/view/view_login.php
@@ -1,18 +1,20 @@
-<?php require_once('partials/header.php'); ?>
+<?php require_once('partials/header.php') ?>
 
 <div class="container-fluid d-flex justify-content-center align-items-center" style="height: 100vh;">
   <div class="card">
     <div class="card-body">
       <h2 class="card-title text-center mb-4">Connexion</h2>
+
+      <!-- afficher le formulaire -->
       <form action="" method="post">
         <div class="form-group mb-3">
-          <label for="email">Votre email</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
+          <label for="emailperso">Votre email</label>
+          <input type="email" class="form-control" id="emailperso" name="emailperso" aria-describedby="emailHelp" placeholder="Saisir votre email" required>
         </div>
 
         <div class="form-group mb-3">
           <label for="mdp">Mot de passe</label>
-          <input type="password" class="form-control" id="mdp" placeholder="Mot de passe" required>
+          <input type="password" class="form-control" id="mdp" name="mdp" placeholder="Saisir votre mot de passe" required>
         </div>
 
         <div class="d-grid">
